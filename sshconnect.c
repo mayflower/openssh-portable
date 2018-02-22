e017d13572d4ba76e1de345579942cabe420a3fc
Robin Gloster <mail@glob.in>
2018-02-22 16:20:48 +0100

    openssl 1.1 support

--- sshconnect.c
+++ sshconnect.c
@@ -1310,6 +1310,7 @@ ssh_login(Sensitive *sensitive, const char *orighost,
 	char *server_user, *local_user;
 
 	local_user = xstrdup(pw->pw_name);
+	free(pw);
 	server_user = options.user ? options.user : local_user;
 
 	/* Convert the user-supplied hostname into all lowercase. */
